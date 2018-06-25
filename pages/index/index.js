9d23b136fef7ca42b44f06517af1e0af8c514226
wangqingpeng <qingpeng.wang@sojex.cn>
2018-06-25 17:59:38 +0800

    点击事件 页面添加

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,53 @@
 //index.js
+const util = require('../../utils/util.js')
 //获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World 1111',
-    number1: 0,
+    currentDate: util.formatDate(new Date()),
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    getFinishCount: 0,
+    todoList: [{
+      todoId: 'cdsdscwcrevvcddfvdf',
+      title: '测试1',
+      isFinish: false,
+      finishCount: 1,
+      createTime: 1529913111000,
+      detail: [{
+          todoDetialId: 'cdsdscdqwdqswdfvdf',
+          title: '测试11',
+          isFinish: true,
+          createTime: 1529913121000,
+        },
+        {
+          todoDetialId: 'cdsqwedcrevvcdedfvdf',
+          title: '测试12',
+          isFinish: false,
+          createTime: 1529913131000,
+        }
+      ]
+    }, {
+      todoId: 'cdgtrgtrewfrevdffvdf',
+      title: '测试2',
+      finishCount: 1,
+      isFinish: false,
+      createTime: 1529913574000
+    }, {
+      todoId: 'cdsdscwc5gythyujnhvdf',
+      title: '测试3',
+      finishCount: 1,
+      isFinish: true,
+      createTime: 1529913556000
+    }, {
+      todoId: 'cdsdscwcnhgbgvcdsdewdf',
+      title: '测试4',
+      finishCount: 1,
+      isFinish: false,
+      createTime: 1529913588000
+    }]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,13 +55,39 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  // 更改当前状态
+  changeStatus(e) {
+    const clickId = e.currentTarget.dataset.id
+    if (!clickId) return
+    const list = this.data.todoList
+    const clickItem = list.filter((el,index) => {
+      return clickId === el.todoId
+    })
+    let clickNo
+    for (var i = 0; i < list.length; i++) {
+      if (list[i].todoId === clickId) {
+        clickNo = i
+        break
+      }
+    }
+    const currentStatus = list[clickNo].isFinish
+    const str = "todoList[" + clickNo + "].isFinish"
+    this.setData({
+      [str]: !currentStatus
+    })
+    wx.vibrateShort()
+  },
+  // 查看详细信心
+  viewDetail(e){
+    console.log(e)
+  },
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,13 +108,5 @@ Page({
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -9,11 +9,18 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+const formatDate = date => {
+  const weekStr = ["天", "一", "二", "三", "四", "五", "六"]
+
+  return `${date.getMonth() + 1}月${date.getDate()}日 星期${weekStr[date.getDay()]}`
+}
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime,
+  formatDate
 }
